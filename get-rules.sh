65ed92b9a09b6a443a1d272dc33c1b97c6016b5c
minhaz33345 <168693030+minhaz33345@users.noreply.github.com>
2024-09-07 07:16:49 +0700

    Update get-rules.sh

--- get-rules.sh
+++ get-rules.sh
@@ -1,4 +1,4 @@
 curl -X GET \
-"https://api.cloudflare.com/client/v4/zones/dc4829573a8be18a7f2de65921259f9e/firewall/rules" \
+"https://api.cloudflare.com/client/v4/zones/0f2a86b8183b24112e4bdce229cb8dbb/firewall/rules" \
 -H "X-Auth-Email: ntuanminh727@gmail.com" \
 -H "X-Auth-Key: 0693a651984730cd20f8679d5ab5d74dcf8ac"
