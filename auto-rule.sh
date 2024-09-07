ca3b8354736cb60627fadaf4f7ca08ab80d99e30
minhaz33345 <168693030+minhaz33345@users.noreply.github.com>
2024-09-07 07:39:31 +0700

    Update auto-rule.sh

--- auto-rule.sh
+++ auto-rule.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 while true
 do
-bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/anti.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/anti.sh)
 sleep 2
 done
