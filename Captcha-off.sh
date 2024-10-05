7b24885e4b4a99ae8f52911b0c463edf7832773e
minhaz33345 <168693030+minhaz33345@users.noreply.github.com>
2024-10-05 22:20:24 +0700

    Update Captcha-off.sh

--- Captcha-off.sh
+++ Captcha-off.sh
@@ -4,7 +4,7 @@ curl -X PUT "https://api.cloudflare.com/client/v4/zones/0f2a86b8183b24112e4bdce2
      -H "Content-Type: application/json" \
      --data '{
      "action": "managed_challenge",
-     "priority": 1200,
+     "priority": 546,
      "paused": true,
      "description": "Managed Challenge Việt Nam",
      "filter": {
