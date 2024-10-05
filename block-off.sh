33aa846255949a0b2a409ae75195f4807ee7e547
minhaz33345 <168693030+minhaz33345@users.noreply.github.com>
2024-10-05 21:58:51 +0700

    Update block-off.sh

--- block-off.sh
+++ block-off.sh
@@ -4,7 +4,7 @@ curl -X PUT "https://api.cloudflare.com/client/v4/zones/0f2a86b8183b24112e4bdce2
      -H "Content-Type: application/json" \
      --data '{
      "action": "block",
-     "priority": 564,
+     "priority": 2100,
      "paused": true,
      "description": "Block Quốc Tế",
      "filter": {
