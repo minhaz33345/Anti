3ce1b2a104deac4d85a35040730f74656d206102
minhaz33345 <168693030+minhaz33345@users.noreply.github.com>
2024-10-06 10:57:12 +0700

    Update block-on.sh

--- block-on.sh
+++ block-on.sh
@@ -4,7 +4,7 @@ curl -X PUT "https://api.cloudflare.com/client/v4/zones/0f2a86b8183b24112e4bdce2
      -H "Content-Type: application/json" \
      --data '{
      "action": "block",
-     "priority": 546,
+     "priority": 500,
      "paused": false,
      "description": "Block Quốc Tế",
      "filter": {
