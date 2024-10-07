da410cd532768825a2e8188851ccd4f11b6c3686
minhaz33345 <168693030+minhaz33345@users.noreply.github.com>
2024-10-08 01:36:35 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -1,9 +1,21 @@
-#!bin/sh
+#!/bin/sh
 connect1=$(netstat -tun | grep ESTABLISHED | wc -l)
 connect2=$(netstat -alntp | grep :443 | wc -l)
-if [ $connect -lt 200 ]
-then
-  cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/block-off.sh) && bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/Captcha-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
+
+if [ $connect1 -gt 200 ] || [ $connect2 -gt 200 ]; then
+  if grep -q "allow" /root/status.txt; then
+    bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/block-on.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/Captcha-on.sh)
+    echo "block" > /root/status.txt
+    clear
+    echo -e "ANTI DDOS ON"
+  fi
 else
-  cat /root/status.txt | grep "allow" && bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/block-on.sh) && bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/Captcha-on.sh) && echo "block" > /root/status.txt && clear && echo -e "ANTI DDOS ON"
+  if grep -q "block" /root/status.txt; then
+    bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/block-off.sh)
+    bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/Captcha-off.sh)
+    echo "allow" > /root/status.txt
+    clear
+    echo -e "ANTI DDOS OFF"
+  fi
 fi
