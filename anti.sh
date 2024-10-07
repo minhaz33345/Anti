e4c6fd6ec4bbc57a7da40e50edf89e117d4ab65d
minhaz33345 <168693030+minhaz33345@users.noreply.github.com>
2024-10-08 00:51:22 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -1,5 +1,6 @@
 #!bin/sh
-connect=$(netstat -tun | grep ESTABLISHED | wc -l)
+connect1=$(netstat -tun | grep ESTABLISHED | wc -l)
+connect2=$(netstat -alntp | grep :443 | wc -l)
 if [ $connect -lt 200 ]
 then
   cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/block-off.sh) && bash <(curl -Ls https://raw.githubusercontent.com/minhaz33345/Anti/main/Captcha-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
