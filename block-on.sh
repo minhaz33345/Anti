bc5200cbb1ff80e6d39d5f4d73b98759d1e65e70
minhaz33345 <168693030+minhaz33345@users.noreply.github.com>
2024-10-08 00:21:31 +0700

    Update block-on.sh

--- block-on.sh
+++ block-on.sh
@@ -1,4 +1,4 @@
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/0f2a86b8183b24112e4bdce229cb8dbb/firewall/rules/c0d9639f8ff34d10866a92bf41e3ee28" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/0f2a86b8183b24112e4bdce229cb8dbb/firewall/rules/c0d9639f8ff34d10866a92bf41e3ee28
      -H "X-Auth-Email: ntuanminh727@gmail.com" \
      -H "X-Auth-Key: 0693a651984730cd20f8679d5ab5d74dcf8ac" \
      -H "Content-Type: application/json" \
